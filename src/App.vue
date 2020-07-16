6791ab43914951246e180e74f85e4f2e898b5cae
tushardk <kshirsagartd@gmail.com>
2020-07-16 20:41:22 +0530

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <Home msg="Welcome to Your Vue.js App" />
+    <HomeSection msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import Home from "./components/Home.vue";
+import HomeSection from "./components/HomeSection.vue";
 
 export default {
   name: "App",
   components: {
-    Home
+    HomeSection
   }
 };
 </script>
