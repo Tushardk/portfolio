ed69bc1c461407b632e4044e4a3a9cb630dfb789
tushardk <kshirsagartd@gmail.com>
2020-08-11 16:22:37 +0530

    vulnerability fix

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,28 @@
 // vue.config.js
 module.exports = {
-    outputDir:'docs',
-    publicPath: process.env.NODE_ENV === 'production'
+  outputDir: 'docs',
+  publicPath: process.env.NODE_ENV === 'production'
     ? '/profile/'
     : '/',
-    filenameHashing : false,
+  filenameHashing: false,
 
-    chainWebpack: (config) => {
-      config
-        .plugin('html')
-        .tap((args) => {
-          args[0].title = 'Tushar Kshirsagar | kshirsagartd@gmail.com | +919503789589 | Front End/UI Developer';
-          return args;
-        });
-    },
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        args[0].title = 'Tushar Kshirsagar | kshirsagartd@gmail.com | +919503789589 | Front End/UI Developer';
+        return args;
+      });
+  },
+  module: {
+    rules: [
+      {
+        test: /package\.json$/,
+        loader: 'package-json-cleanup-loader',
+        options: {
+          only: ['version', 'name', 'otherParam']
+        }
+      }
+    ]
   }
+}
